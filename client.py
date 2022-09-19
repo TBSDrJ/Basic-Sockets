132443a1d16959f0aa5f89cbf9666468f3e35b07
TBSDrJ <marcus.jaiclin@bishops.com>
2022-09-18 21:50:43 -0700

    Fix PORT should be integer

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
 HOST = "127.0.0.1" #localhost
-PORT = "8008" #Anything unused over 1023 is good
+PORT = 8008 #Anything unused over 1023 is good
 
 kb_sent = 0
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(HOST, PORT)
+    s.connect((HOST, PORT))
     with open('test.data', 'rb') as f:
         while True:
             data = f.read(1024)
@@ -14,4 +14,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 kb_sent += 1
             else:
                 break
+                
 print(f"Socket closed. Send {kb_sent}kb of data.")
